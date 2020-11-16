70125ba34be28391e4d5fd50de34c11f2da97679
Andrei Jung <jung.andrei@gmail.com>
2020-11-16 09:17:10 -0300

    Sessão 9

---
+++ EnumEx/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace EnumEx.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+        PendingPaument = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
---
+++ EnumEx/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using EnumEx.Entities;
+using EnumEx.Entities.Enums;
+
+namespace EnumEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPaument
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPaument.ToString();
+            Console.WriteLine(txt);
+
+            //Convertendo string para enum
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(os);
+
+        }
+    }
+}
