51e68d61df7c67d0bf8c6f3a884466ca4c4af9e1
Andrei Jung <jung.andrei@gmail.com>
2020-11-17 10:12:09 -0300

    s

---
+++ StringBuildEx/Entities/Post.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Principal;
+using System.Text;
+
+namespace StringBuildEx.Entities
+{
+    class Post
+    {
+        public DateTime Moment { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public int Likes { get; set; }
+
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public Post()
+        {
+
+        }
+
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+
+        public void AddComment(Comment comment)
+        {
+            Comments.Add(comment);   //Adiciona nos comentarios o comentario
+        }
+
+        public void RemoveComment(Comment comment)
+        {
+            Comments.Remove(comment);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(Title);
+            sb.Append(Likes);
+            sb.Append(" Likes - ");
+            sb.AppendLine(Moment.ToString("G"));
+            sb.AppendLine(Content);
+            sb.AppendLine("Comments: ");
+            foreach (Comment c in Comments)
+            {
+                sb.AppendLine(c.Text);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ StringBuildEx/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Security.Claims;
+using StringBuildEx.Entities;
+
+namespace StringBuildEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 INSTANCIA
+            Comment c1 = new Comment("Have a nice trip");
+            Comment c2 = new Comment("Wow that's awesome!");
+
+            //Populando o construtor do Post
+            Post p1 = new Post(
+                DateTime.Parse("21/06/2018 13:05:44"),
+                "Traveling to new Zealand",
+                "I'm going to visit this wonderful country!",
+                12);
+
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+            //2 INSTANCIA
+            Comment c3 = new Comment("Good night");
+            Comment c4 = new Comment("May the Force be with you");
+
+            //Populando o construtor do Post
+            Post p2 = new Post(
+                DateTime.Parse("28/07/2018 23:14:19"),
+                "Good night",
+                "See you tomorrow",
+                5);
+
+            p1.AddComment(c3);
+            p1.AddComment(c4);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+        }
+    }
+}
