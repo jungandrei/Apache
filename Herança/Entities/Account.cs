8176df1a5189d413a85c6862f0cd24f095525a58
Andrei Jung <jung.andrei@gmail.com>
2020-11-17 15:22:54 -0300

    s

---
+++ Herança/Entities/Account.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Herança.Entities
+{
+    class Account
+    {
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; protected set; }
+
+        public Account()
+        {
+
+        }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public void WithDraw(double amount)
+        {
+            Balance = Balance - amount;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance = Balance + amount;
+        }
+
+    }
+}
---
+++ Herança/Entities/BusinessAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Herança.Entities
+{
+    class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount()
+        {
+
+        }
+
+        //BASE - Pegando os atributos do construtor pai
+
+        public BusinessAccount(int number, string holder, double balance, double loanLimit) 
+            : base( number, holder, balance)
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if (LoanLimit <= amount)
+            {
+                Balance = Balance + amount;
+            }
+        }
+    }
+}
